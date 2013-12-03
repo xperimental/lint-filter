47655e599f94719d50b80daff2a1ec3454ae440a
Robert Jacob <xperimental@solidproject.de>
2013-12-03 20:27:34 +0100

    Added XML output.

--- lint-filter.go
+++ lint-filter.go
@@ -5,13 +5,23 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"os"
 )
 
 func main() {
 	var configPath = flag.String("config", "filterconfig.json", "Path to filter configuration file.")
+	var prettyXml = flag.Bool("pretty", false, "If present, XML output will be indented.")
 	flag.Parse()
 
 	input := flag.Arg(0)
+	output := flag.Arg(1)
+
+	if len(input) == 0 || len(output) == 0 {
+		fmt.Print("usage: lint-filter [options] input-file output-file\n\nOptions:\n")
+		flag.PrintDefaults()
+		return
+	}
+
 	fmt.Printf("Input: %s\n", input)
 	xmlContent, err := ioutil.ReadFile(input)
 	if err != nil {
@@ -48,4 +58,22 @@ func main() {
 		}
 	}
 	fmt.Printf("issues after filter: %d\n", len(filteredIssues))
+
+	lintReport.Issues = filteredIssues
+
+	outputFile, err := os.Create(output)
+	if err != nil {
+		fmt.Printf("Error opening output: %s", err)
+		return
+	}
+	outputFile.WriteString(xml.Header)
+	encoder := xml.NewEncoder(outputFile)
+	if *prettyXml {
+		encoder.Indent("", "  ")
+	}
+	err = encoder.Encode(lintReport)
+	if err != nil {
+		fmt.Printf("Error writing XML: %s", err)
+	}
+	outputFile.Close()
 }
